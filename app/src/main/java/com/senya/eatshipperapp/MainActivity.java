7fdfa4689eeaffc0565064d37dd8e7f817b2f824
sen9ka <ars-shvec@yandex.ru>
2022-05-08 18:48:27 +0300

    Вход рега

---
+++ app/src/main/java/com/senya/eatshipperapp/MainActivity.java
@@ -0,0 +1,197 @@
+package com.senya.eatshipperapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.senya.eatshipperapp.common.Common;
+import com.senya.eatshipperapp.model.ShipperUserModel;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int APP_REQUEST_CODE = 7171;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener listener;
+    private AlertDialog dialog;
+    private DatabaseReference serverRef;
+    private List<AuthUI.IdpConfig> providers;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(listener);
+    }
+
+    @Override
+    protected void onStop() {
+        if(listener != null)
+            firebaseAuth.removeAuthStateListener(listener);
+        super.onStop();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init();
+    }
+
+    private void init() {
+        providers = Arrays.asList(new AuthUI.IdpConfig.PhoneBuilder().build());
+
+        serverRef = FirebaseDatabase.getInstance().getReference(Common.SHIPPER_REF);
+        firebaseAuth = FirebaseAuth.getInstance();
+        dialog = new AlertDialog.Builder(MainActivity.this).create();
+        listener = firebaseAuthLocal -> {
+            FirebaseUser user = firebaseAuthLocal.getCurrentUser();
+            if(user != null)
+            {
+                //проверка пользователя из БД
+                checkServerUserFromFirebase(user);
+            }
+            else
+            {
+                phoneLogin();
+            }
+        };
+    }
+
+    private void checkServerUserFromFirebase(FirebaseUser user) {
+        dialog.show();
+        serverRef.child(user.getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.exists())
+                        {
+                            ShipperUserModel userModel = snapshot.getValue(ShipperUserModel.class);
+                            if(userModel.isActive())
+                            {
+                                goToHomeActivity(userModel);
+                            }
+                            else
+                            {
+                                dialog.dismiss();
+                                Toast.makeText(MainActivity.this, "Wait for the approval", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else
+                        {
+                            //пользователя нет в БД
+                            dialog.dismiss();
+                            showRegisterDialog(user);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        dialog.dismiss();
+                        Toast.makeText(MainActivity.this, ""+error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void showRegisterDialog(FirebaseUser user) {
+        androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(this);
+        builder.setTitle("Register");
+        builder.setMessage("Fill In your Information \n It will be accepted later");
+
+        View itemView = LayoutInflater.from(this).inflate(R.layout.layout_register,null);
+        EditText edt_name = (EditText) itemView.findViewById(R.id.edt_name);
+        EditText edt_phone = (EditText) itemView.findViewById(R.id.edt_phone);
+
+        //Впис инф
+        edt_phone.setText(user.getPhoneNumber());
+        builder.setNegativeButton("Cancel", (dialogInterface, i) -> dialogInterface.dismiss())
+                .setPositiveButton("Register", (dialogInterface, i) -> {
+                    if(TextUtils.isEmpty(edt_name.getText().toString()))
+                    {
+                        Toast.makeText(MainActivity.this, "Enter your name", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    ShipperUserModel shipperUserModel = new ShipperUserModel();
+                    shipperUserModel.setUid(user.getUid());
+                    shipperUserModel.setName(edt_name.getText().toString());
+                    shipperUserModel.setPhone(edt_phone.getText().toString());
+                    shipperUserModel.setActive(false); //по дефолту отключен включается в БД
+
+                    dialog.show();
+
+                    serverRef.child(shipperUserModel.getUid())
+                            .setValue(shipperUserModel)
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    dialog.dismiss();
+                                    Toast.makeText(MainActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            dialog.dismiss();
+                            Toast.makeText(MainActivity.this, "Successfully registered. Wait for the approval", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                });
+
+        builder.setView(itemView);
+
+        androidx.appcompat.app.AlertDialog registerDialog = builder.create();
+        registerDialog.show();
+    }
+
+    private void goToHomeActivity(ShipperUserModel serverUserModel) {
+        dialog.dismiss();
+        Common.currentShipperUser = serverUserModel;
+        startActivity(new Intent(this,HomeActivity.class));
+        finish();
+    }
+
+    private void phoneLogin() {
+        startActivityForResult(AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),APP_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == APP_REQUEST_CODE)
+        {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if(resultCode == RESULT_OK)
+            {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            }
+            else
+            {
+                Toast.makeText(this, "Failed to Sign In", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/senya/eatshipperapp/common/Common.java
@@ -0,0 +1,123 @@
+package com.senya.eatshipperapp.common;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.senya.eatshipperapp.R;
+import com.senya.eatshipperapp.model.ShipperUserModel;
+import com.senya.eatshipperapp.model.TokenModel;
+
+public class Common {
+
+    public static final String SHIPPER_REF = "Shippers";
+    public static final String CATEGORY_REF = "Category";
+    public static final String ORDER_REF = "Order";
+    public static ShipperUserModel currentShipperUser;
+    public static final String NOTI_TITLE = "title";
+    public static final String NOTI_CONTENT = "content";
+    public static final String TOKEN_REF = "Tokens";
+
+    public static final int DEFAULT_COLUMN_COUNT = 0;
+    public static final int FULL_WIDTH_COLUMN = 1;
+
+    public static void setSpanString(String welcome, String name, TextView textView) {
+        SpannableStringBuilder builder = new SpannableStringBuilder();
+        builder.append(welcome);
+        SpannableString spannableString = new SpannableString(name);
+        StyleSpan boldSpan = new StyleSpan(Typeface.BOLD);
+        spannableString.setSpan(boldSpan,0,name.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        builder.append(spannableString);
+        textView.setText(builder,TextView.BufferType.SPANNABLE);
+    }
+
+    public static void setSpanStringColor(String welcome, String name, TextView textView, int color) {
+        SpannableStringBuilder builder = new SpannableStringBuilder();
+        builder.append(welcome);
+        SpannableString spannableString = new SpannableString(name);
+        StyleSpan boldSpan = new StyleSpan(Typeface.BOLD);
+        spannableString.setSpan(boldSpan,0,name.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ForegroundColorSpan(color),0,name.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        builder.append(spannableString);
+        textView.setText(builder,TextView.BufferType.SPANNABLE);
+    }
+
+    public static String convertStatusToString(int orderStatus) {
+        switch (orderStatus)
+        {
+            case 0:
+                return "Placed";
+            case 1:
+                return "Shipping";
+            case 2:
+                return "Shipped";
+            case -1:
+                return "Cancelled";
+            default:
+                return "Error";
+        }
+    }
+
+    public static void showNotification(Context context, int id, String title, String content, Intent intent) {
+        PendingIntent pendingIntent = null;
+        if(intent != null)
+            pendingIntent = PendingIntent.getActivity(context,id,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        String NOTIFICATION_CHANNEL_ID = "senya_eat_appie";
+        NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,
+                    "Eat App", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("Eat App");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0,1000,500,1000});
+            notificationChannel.enableVibration(true);
+
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context,NOTIFICATION_CHANNEL_ID);
+        builder.setContentTitle(title)
+                .setContentText(content)
+                .setAutoCancel(true)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setLargeIcon(BitmapFactory.decodeResource(Resources.getSystem(), R.drawable.ic_baseline_restaurant_menu_24));
+        if(pendingIntent != null)
+            builder.setContentIntent(pendingIntent);
+        Notification notification = builder.build();
+        notificationManager.notify(id,notification);
+    }
+
+    public static void updateToken(Context context, String newToken) {
+        FirebaseDatabase.getInstance()
+                .getReference(Common.TOKEN_REF)
+                .child(Common.currentShipperUser.getUid())
+                .setValue(new TokenModel(Common.currentShipperUser.getPhone(), newToken))
+                .addOnFailureListener(e -> {
+                    Toast.makeText(context, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+    }
+
+    public static String createTopicOrder() {
+        return new StringBuilder("/topics/new_order").toString();
+    }
+
+}
